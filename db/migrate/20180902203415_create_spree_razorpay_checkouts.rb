88a70867e84f3fba73905f9ca7fb190dcb280e03
Neeraj Kumar <neeraj.kumar@gmail.com>
2018-09-05 02:47:00 +0530

    Adding additional fields into razorpay_checkouts table

--- db/migrate/20180902203415_create_spree_razorpay_checkouts.rb
+++ db/migrate/20180902203415_create_spree_razorpay_checkouts.rb
@@ -2,6 +2,15 @@ class CreateSpreeRazorpayCheckouts < ActiveRecord::Migration
   def change
     create_table :spree_razorpay_checkouts do |t|
       t.string :order_id
+      t.string :razorpay_payment_id
+      t.string :status
+      t.string :payment_method
+      t.string :card_id
+      t.string :bank
+      t.string :wallet
+      t.string :vpa
+      t.string :email
+      t.string :contact
     end
   end
 end
