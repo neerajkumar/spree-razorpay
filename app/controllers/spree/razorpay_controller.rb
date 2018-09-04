046b9e2234d14b345fefe13fafc0cadc04e60cfe
Neeraj Kumar <neeraj.kumar@gmail.com>
2018-09-04 16:21:17 +0530

    loading razorpay in controller now.

--- app/controllers/spree/razorpay_controller.rb
+++ app/controllers/spree/razorpay_controller.rb
@@ -1,3 +1,5 @@
+require 'razorpay'
+
 module Spree
   class RazorpayController < StoreController
 
@@ -8,6 +10,8 @@ module Spree
     end
 
     def purchase_status
+      payment_method = Spree::PaymentMethod.find(params[:payment_method_id])
+      Razorpay.setup(payment_method.preferences[:key_id], payment_method.preferences[:key_secret])
       @order = Spree::Order.process_razorpayment(params)
       redirect_to order_path(@order)
     end
--- lib/spree_razorpay.rb
+++ lib/spree_razorpay.rb
@@ -1,6 +1,5 @@
 require 'spree_core'
 require 'spree_razorpay/engine'
-require 'razorpay'
 
 module SpreeRazorpay
 end
